568fb620175e4cd24232198bd7714d0e7f1421bc
Dilshaan <dilshaan.sandhu6@gmail.com>
2023-01-30 13:26:38 -0700

    2023-01-30

--- jobScraper.py
+++ jobScraper.py
@@ -7,7 +7,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from pytz import timezone
 from selenium import webdriver
-from selenium.common import ElementNotInteractableException, ElementClickInterceptedException
+from selenium.common import ElementNotInteractableException, ElementClickInterceptedException, NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
@@ -34,7 +34,8 @@ class jobScraper:
         if not broswer_vis:
             option.add_argument("--window-size=1920,1080")
             option.add_argument("--headless")
-            user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+            user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                         'Chrome/108.0.0.0 Safari/537.36 '
             option.add_argument(f'user-agent={user_agent}')
 
         # Get the country/state/city from the list of countries, set the URL, Scrape, then save
@@ -56,102 +57,115 @@ class jobScraper:
         time.sleep(self.time_out)
 
         # Click on the Jobs tab and select the list we will scroll
-        self.driver.find_element(By.ID, "fMGJ3e").click()
-        job_list = self.driver.find_element(By.CLASS_NAME, 'zxU94d')
 
-        # Contain all the data saved to the csv
-        all_job_data = list()
+        try:
 
-        li_count = 0
+            self.driver.find_element(By.ID, "fMGJ3e").click()
+            job_list = self.driver.find_element(By.CLASS_NAME, 'zxU94d')
+            # Contain all the data saved to the csv
+            all_job_data = list()
 
-        while self.num_links_needed > self.links_collected:
+            li_count = 0
 
-            # Finds multiple lists of jobs in the scrollable div
-            li_focus = self.driver.find_elements(By.CLASS_NAME, "nJXhWc")
+            while self.num_links_needed > self.links_collected:
 
-            # Checks if we reached the end of the scrollable div
-            if len(li_focus) < li_count + 1:
-                break
+                # Finds multiple lists of jobs in the scrollable div
+                li_focus = self.driver.find_elements(By.CLASS_NAME, "nJXhWc")
 
-            # Clicks on the job postings in the li and stores the data
-            for li in li_focus[li_count].find_elements(By.TAG_NAME, "li"):
+                # Checks if we reached the end of the scrollable div
+                if len(li_focus) < li_count + 1:
+                    break
 
-                post_data = {}
-                try:
-                    li.click()
+                # Clicks on the job postings in the li and stores the data
+                for li in li_focus[li_count].find_elements(By.TAG_NAME, "li"):
 
-                except (ElementNotInteractableException, ElementClickInterceptedException):
-                    self.driver.execute_script(
-                        f'arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', job_list
-                    )
+                    post_data = {}
+                    try:
+                        li.click()
 
+                    except (ElementNotInteractableException, ElementClickInterceptedException):
+                        self.driver.execute_script(
+                            f'arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', job_list
+                        )
+
+                    time.sleep(self.time_out)
+
+                    # Get HTML of only specific job posting
+                    bSoup = BeautifulSoup(self.driver.find_element(By.ID, "tl_ditsc").get_attribute("outerHTML"),
+                                          'html.parser')
+
+                    jobDetail = bSoup.findAll("div", {"class": "I2Cbhb"})
+
+                    # Default values
+                    post_data["Job-Type"] = "NA"
+                    post_data["Date-Posted"] = "NA"
+                    post_data["Salary"] = "NA"
+
+                    post_data["Job-Title"] = bSoup.find("h2").text
+                    post_data["Date-Scraped"] = f"{datetime.now(timezone(self.timezone)).strftime('%Y-%m-%d %H:%M:%S')}"
+                    # Gets the job type, date posted, and salary
+                    for element in jobDetail:
+
+                        if element.text in ("Full-time", "Part-time", "Internship", "Contractor"):
+
+                            post_data["Job-Type"] = element.text
+
+                        elif "ago" in element.text:
+
+                            if "day" in element.text:
+
+                                post_data["Date-Posted"] = (
+                                    (datetime.now() - timedelta(days=(int(element.text[0:2].strip())))).astimezone(
+                                        timezone(self.timezone)).strftime("%Y-%m-%d"))
+
+                            else:
+                                post_data["Date-Posted"] = datetime.now().astimezone(timezone(self.timezone)).strftime(
+                                    "%Y-%m-%d")
+                                print(post_data["Date-Posted"])
+
+                        elif "a year" in element.text:
+                            post_data["Salary"] = element.text
+
+                    post_data["Url"] = self.driver.current_url
+                    post_data["Company"] = bSoup.findAll("div", {"class": "nJlQNd"})[0].text
+                    post_data["Location"] = bSoup.findAll("div", {"class": "sMzDkb"})[1].text
+                    post_data["Description"] = bSoup.find("span",
+                                                          {"class": "HBvzbc"}).text
+
+                    self.links_collected += 1
+                    all_job_data.append(post_data)
+
+                # Scroll down to the next list of jobs
+                self.driver.execute_script(
+                    f'arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', job_list
+                )
+
+                li_count += 1
                 time.sleep(self.time_out)
 
-                # Get HTML of only specific job posting
-                bSoup = BeautifulSoup(self.driver.find_element(By.ID, "tl_ditsc").get_attribute("outerHTML"),
-                                      'html.parser')
+                # Tracks progress
+                print(f"{self.links_collected} / {self.num_links_needed}")
 
-                jobDetail = bSoup.findAll("div", {"class": "I2Cbhb"})
+            print(all_job_data)
+            return all_job_data
 
-                # Default values
-                post_data["Job-Type"] = "NA"
-                post_data["Date-Posted"] = "NA"
-                post_data["Salary"] = "NA"
+        except NoSuchElementException:
+            return None
 
-                post_data["Job-Title"] = bSoup.find("h2").text
-                post_data["Date-Scraped"] = f"{datetime.now(timezone(self.timezone)).strftime('%Y-%m-%d %H:%M:%S')}"
-                # Gets the job type, date posted, and salary
-                for element in jobDetail:
 
-                    if element.text in ("Full-time", "Part-time", "Internship", "Contractor"):
 
-                        post_data["Job-Type"] = element.text
-
-                    elif "ago" in element.text:
-
-                        if "day" in element.text:
-
-                            post_data["Date-Posted"] = (
-                                (datetime.now() - timedelta(days=(int(element.text[0:2].strip())))).astimezone(
-                                    timezone(self.timezone)).strftime("%Y-%m-%d"))
-
-                        else:
-                            post_data["Date-Posted"] = datetime.now().astimezone(timezone(self.timezone)).strftime(
-                                "%Y-%m-%d")
-                            print(post_data["Date-Posted"])
-
-                    elif "a year" in element.text:
-                        post_data["Salary"] = element.text
-
-                post_data["Url"] = self.driver.current_url
-                post_data["Company"] = bSoup.findAll("div", {"class": "nJlQNd"})[0].text
-                post_data["Location"] = bSoup.findAll("div", {"class": "sMzDkb"})[1].text
-                post_data["Description"] = bSoup.find("span",
-                                                      {"class": "HBvzbc"}).text
-
-                self.links_collected += 1
-                all_job_data.append(post_data)
-
-            # Scroll down to the next list of jobs
-            self.driver.execute_script(
-                f'arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', job_list
-            )
-
-            li_count += 1
-            time.sleep(self.time_out)
-
-            # Tracks progress
-            print(f"{self.links_collected} / {self.num_links_needed}")
-
-        print(all_job_data)
-        return all_job_data
 
     def saveToCsv(self, data, country):
         """"Saves the data to a csv file and overwrites any previous data"""
 
-        df = pd.DataFrame(data,
-                          columns=["Job-Title", "Date-Posted", "Date-Scraped", "Url", "Company", "Job-Type", "Salary",
-                                   "Location", "Description"])
-        df.to_csv(f'data/unprocessedData/machineLearningJobData{country}.csv', mode='w', index=False)
-        print("Data Saved")
-        print(df)
+        if data != None:
+
+            df = pd.DataFrame(data,
+                              columns=["Job-Title", "Date-Posted", "Date-Scraped", "Url", "Company", "Job-Type", "Salary",
+                                       "Location", "Description"])
+            df.to_csv(f'data/unprocessedData/machineLearningJobData{country}.csv', mode='w', index=False)
+            print("Data Saved")
+            print(df)
+
+        else:
+            print("No Data Saved")
--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-from jobScraper import jobScraper as js
 from jobProcessing import jobProcessor as jp
+from jobScraper import jobScraper as js
 
 # Number of jobs needed to be scraped.
-num_of_jobs_needed = 1000
+num_of_jobs_needed = 100000
 
 # If we run out of coutries_states to scrape, then the program will stop, regardless of the number_of_jobs.
-countries_or_states = ["United States California", "India", "United Kingdom London", "Canada Toronto",
-                       "Australia Sydney", "New Zealand", "Singapore", "Malaysia", "Hong Kong", "South Africa",
-                       "Ireland", "Germany", "France", "Spain", "Italy", "Norway", "Denmark", "Finland",
-                       "Netherlands", "Belgium", "Austria", "Switzerland", "Poland",
-                       "Russia", "Turkey", "Brazil", "Mexico", "Argentina", "Chile", "Venezuela",
-                       "Ecuador", "Uruguay", "Bolivia", "Costa Rica", "Panama", "Dominican Republic",
-                       "Puerto Rico", "Pakistan", "Afghanistan"]
+countries_or_states = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua & Deps", "Argentina",
+                       "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados",
+                       "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia Herzegovina", "Botswana",
+                       "Brazil", "Brunei", "Bulgaria", "Burkina", "Burundi", "Cambodia", "Cameroon", "Canada",
+                       "Cape Verde", "Central African Rep", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo",
+                       "Congo {Democratic Rep}", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark",
+                       "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador",
+                       "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon",
+                       "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea",
+                       "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia",
+                       "Iran", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan",
+                       "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea North", "Korea South", "Kosovo", "Kuwait",
+                       "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein",
+                       "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali",
+                       "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova",
+                       "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar, {Burma}", "Namibia",
+                       "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway",
+                       "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines",
+                       "Poland", "Portugal", "Qatar", "Romania", "Russian Federation", "Rwanda", "St Kitts & Nevis",
+                       "St Lucia", "Saint Vincent & the Grenadines", "Samoa", "San Marino", "Sao Tome & Principe",
+                       "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia",
+                       "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka",
+                       "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan",
+                       "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad & Tobago", "Tunisia", "Turkey",
+                       "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom",
+                       "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam",
+                       "Yemen", "Zambia", "Zimbabwe"]
 
 # Timezone
 scraping_timezone = "Canada/Mountain"
@@ -23,19 +43,18 @@ topic = "Machine Learning".replace(" ", "+")
 driver_path = r"C:\Users\dilsh\Downloads\chromedriver_win32\chromedriver.exe"  # Todo("CHANGE THIS TO UR PATH! so it looks like driver_path = 'YOUR STRING' ")
 
 # time out needed between events, based on Wi-Fi and PC performance
-time_out = 0.5
+time_out = 0.3
 
 # Boolean which controls if the browser activities will be shown on screen on or not.
 browser_visible = False
 
 # Scrape/Process? or only process
-type = "process"
-
+type = "scrapeprocess"
 
 if __name__ == "__main__" and type == "scrapeprocess":
-     JobScraper = js(topic, countries_or_states, driver_path, num_of_jobs_needed, time_out, browser_visible,
-                            scraping_timezone)
-     jp()
+    JobScraper = js(topic, countries_or_states, driver_path, num_of_jobs_needed, time_out, browser_visible,
+                    scraping_timezone)
+    jp()
 
 else:
     jp()
